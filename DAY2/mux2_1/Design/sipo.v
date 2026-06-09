76748fff4ea437bd5d32ddaceb488a9a3c5a8837
Ambu1235 <prayagvt40@gmail.com>
2026-06-09 20:34:06 +0530

    Update sipo.v

--- DAY2/mux2_1/Design/sipo.v
+++ DAY2/mux2_1/Design/sipo.v
@@ -1,1 +1,21 @@
+module sipo(input clk,rst,sin,enb, output reg [3:0]p
+
+    );
+    reg [3:0]temp;
+    always @(posedge clk)
+    begin
+    if (rst) begin
+    temp<=4'b0000;
+    end
+    else if(enb==0) begin
+    temp<=temp>>1'b1;
+    temp[3]<=sin;
+    end
+    else if(enb==1) begin
+    p<=temp;
+    end
+    
+    
+    end
+endmodule
 
